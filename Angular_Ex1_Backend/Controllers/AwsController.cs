d1b088a7238d5f6ee106c2ff3d4f20da33303ca4
alunaya <33818729+alunaya@users.noreply.github.com>
2021-01-22 15:16:27 +0700

    Add project files.

---
+++ Angular_Ex1_Backend/Controllers/AwsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+
+namespace Angular_Ex1_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AwsController : ControllerBase
+    {
+        public readonly AmazonEC2Client ec2Client;
+
+        public AwsController(AmazonEC2Client ec2Client)
+        {
+            this.ec2Client = ec2Client ?? throw new ArgumentNullException(nameof(ec2Client));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> getInstanceTypeList()
+        {
+            DescribeInstanceTypeOfferingsRequest request = new DescribeInstanceTypeOfferingsRequest();
+            var result = await ec2Client.DescribeInstanceTypeOfferingsAsync(request);
+            return new JsonResult(result.InstanceTypeOfferings.Select(x=>x.InstanceType.Value));
+        }
+    }
+}
---
+++ Angular_Ex1_Backend/SeedData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_Ex1_Backend
+{
+    public static class SeedData
+    {
+        public static void SeedDatabase(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
